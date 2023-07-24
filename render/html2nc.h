39fb7a88e8a611692c87a112eee982976aa28cc8
Lord-McSweeney <batuharmanci@gmail.com>
2023-07-24 06:06:28 -0700

    render: Support basic HTML entities

--- render/html2nc.h
+++ render/html2nc.h
@@ -11,12 +11,73 @@ int isBlock(struct xml_node *node) {
         return 1;
     }
     char *lower = xml_toLowerCase(node->name);
-    return !strcmp(lower, "div") || !strcmp(lower, "ul") || !strcmp(lower, "p") || !strcmp(lower, "hr") || !strcmp(lower, "header") || !strcmp(lower, "aside") || !strcmp(lower, "h1") || !strcmp(lower, "h2") || !strcmp(lower, "h3") || !strcmp(lower, "h4") || !strcmp(lower, "h5") || !strcmp(lower, "h6") || !strcmp(lower, "blockquote") || !strcmp(lower, "dt");
+    return !strcmp(lower, "div") || !strcmp(lower, "ul") || !strcmp(lower, "p") || !strcmp(lower, "hr") || !strcmp(lower, "header") || !strcmp(lower, "aside") || !strcmp(lower, "h1") || !strcmp(lower, "h2") || !strcmp(lower, "h3") || !strcmp(lower, "h4") || !strcmp(lower, "h5") || !strcmp(lower, "h6") || !strcmp(lower, "blockquote") || !strcmp(lower, "dt") || !strcmp(lower, "tspan");
 }
 
 int isInline(char *nodeName) {
     char *lower = xml_toLowerCase(nodeName);
     return !strcmp(lower, "a") || !strcmp(lower, "span");
+}
+
+// NOTE: This will free() the `content` passed into it
+char *parseHTMLEscapes(const char *content) {
+    char *allocated = (char *) calloc(strlen(content) + 1, sizeof(char));
+    int len = strlen(content);
+    int realIndex = 0;
+    for (int i = 0; i < len; i ++) {
+        char curChar = content[i];
+
+        if (curChar == '&') {
+            if (!strncmp(content + i + 1, "lt", 2)) {
+                allocated[realIndex] = '<';
+                i += 3;
+                realIndex ++;
+                if (content[i] != ';') i --;
+            } else if (!strncmp(content + i + 1, "gt", 2)) {
+                allocated[realIndex] = '>';
+                i += 3;
+                realIndex ++;
+                if (content[i] != ';') i --;
+            } else if (!strncmp(content + i + 1, "amp", 3)) {
+                allocated[realIndex] = '&';
+                i += 4;
+                realIndex ++;
+                if (content[i] != ';') i --;
+            } else if (!strncmp(content + i + 1, "nbsp", 4)) {
+                allocated[realIndex] = ' ';
+                i += 5;
+                realIndex ++;
+                if (content[i] != ';') i --;
+            } else if (!strncmp(content + i + 1, "#305", 4)) {
+                // display-handling doesn't support this, unfortunately
+                strcat(allocated, "ı");
+                i += 5;
+                realIndex += strlen("ı");
+                if (content[i] != ';') i --;
+            } else if (!strncmp(content + i + 1, "copy", 4)) {
+                // display-handling doesn't support the actual non-ascii symbols (copyright), unfortunately
+                allocated[realIndex] = 'C';
+                i += 5;
+                realIndex ++;
+                if (content[i] != ';') i --;
+            } else if (!strncmp(content + i + 1, "raquo", 5)) {
+                // display-handling doesn't support these, unfortunately
+                allocated[realIndex] = 0xbb;
+                i += 6;
+                realIndex ++;
+                if (content[i] != ';') i --;
+            } else {
+                allocated[realIndex] = curChar;
+                realIndex ++;
+            }
+        } else {
+            allocated[realIndex] = curChar;
+            realIndex ++;
+        }
+    }
+    char *newText = XML_makeStrCpy(allocated);
+    free(allocated);
+    return newText;
 }
 
 char *getXMLTrimmedTextContent(char *content, int removeStart) {
@@ -68,7 +129,9 @@ char *recursiveXMLToText(struct xml_node *parent, struct xml_list xml, struct ht
         if (node.type == NODE_DOCTYPE) {
             // ignore
         } else if (node.type == NODE_TEXT) {
-            strcat(alloc, getXMLTrimmedTextContent(node.text_content, justHadInlineInsideBlockWithText));
+            char *text = getXMLTrimmedTextContent(node.text_content, justHadInlineInsideBlockWithText);
+            strcat(alloc, parseHTMLEscapes(text));
+            free(text);
             //printf(strcat(getTabsRepeated(depth), "text node (#%d). contents: '%s'\n"), i, node.text_content);
             //printf(strcat(getTabsRepeated(depth), "text node (#%d). contents: '...'\n"), i);
         } else if (node.type == NODE_COMMENT) {
