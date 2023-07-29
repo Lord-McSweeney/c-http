de54b91defaafde535b6575ba6a7d1f81347f10f
Lord-McSweeney <batuharmanci@gmail.com>
2023-07-29 09:16:36 -0700

    render: Set cursor to none when displaying text

--- render/display-handling.h
+++ render/display-handling.h
@@ -281,7 +281,7 @@ void render_nc(struct nc_state *browserState) {
     for (int i = 0; i < numTexts; i ++) {
         if (browserState->texts[i].visible) {
             printText(browserState->texts[i].y, browserState->texts[i].x, browserState->texts[i].text);
-            //mvprintw();
+            curs_set(0);
         }
     }
     for (int i = 0; i < numButtons; i ++) {
